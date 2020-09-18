12e95d42efb04da5481063a411d172057b820d24
Abhishek Awasthi <justflyhard@gmail.com>
2020-09-19 00:19:02 +0530

    Update search.js

--- search.js
+++ search.js
@@ -3,11 +3,9 @@ import {getGoogleSearch} from './apihandler'
 export async function google_search(content){
     try{
         content = content.toLowerCase()
-        const results = await getGoogleSearch(content)
-        console.log(JSON.stringify(results.data.items.length))
-        const top_five_items = results.data.items.slice(0,5).map(i => i.link)
+        const results = await getGoogleSearch(content) // calling the google search api
+        const top_five_items = results.data.items.slice(0,5).map(i => i.link) //taking top 5 results from google search
         const message = top_five_items.reduce((acc,link) => acc + link + "\n","")
-        console.log(message)
         return message
     }catch(error){
         return "error occured"
